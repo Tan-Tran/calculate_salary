ec1d5fbbc71bf32f2bccadc59f7d4bf220696581
Tan-Tran <tantranminh2803@gmail.com>
2022-03-10 13:17:41 +0700

    complete solution2

--- src/functions/solution2/calculateNetToGrossByFormula.js
+++ src/functions/solution2/calculateNetToGrossByFormula.js
@@ -5,36 +5,51 @@ import {defaultInsurance} from '../../data/data'
 
 export const calculateNetToGrossByFormula = ({income, insurance, reduction}) => {
     const netSalary =  totalGrossSalaryVnd(income)
-    const valueToCalculateInsurance = insurance.fullWage? totalGrossSalaryVnd(income): +insurance.other
     const totalReduction = totalReductionFamily(reduction)
     const convertIncome = netSalary - totalReduction
+    const maximumInsuranceBaseOnMinimumWage = insurance.minimumWage * defaultInsurance.factor
+    const incomeBeforeTax = calculateIncomeBeforeTax(convertIncome)
+    const maxGrossBaseOnMaximumUnEmployed = insurance.region.maximumUnemployedInsurance * 100
+    const socialInsurancePercent = +insurance.socialPercent;
+    const healthInsurancePercent = +insurance.healthPercent;
+    const unEmployedInsurancePercent =  +insurance.unEmployedPercent;
     if(insurance.fullWage){
-        if(convertIncome > 0){
-            const maximumInsuranceBaseOnMinimumWage = insurance.minimumWage * defaultInsurance.factor
-            const incomeBeforeTax = Math.ceil(calculateIncomeBeforeTax(convertIncome))
+        if(convertIncome > 0){            
             if(incomeBeforeTax < maximumInsuranceBaseOnMinimumWage){
-                return (incomeBeforeTax + totalReduction) / (1-((insurance.socialPercent + insurance.healthPercent + insurance.unEmployedPercent)/100))
-            }
-            const maxGrossBaseOnMaximumUnEmployed = insurance.region.maximumUnemployedInsurance * 100
-            if(netSalary < maxGrossBaseOnMaximumUnEmployed){
-                return (incomeBeforeTax + totalReduction + maximumInsuranceBaseOnMinimumWage * ((insurance.socialPercent + insurance.healthPercent)/100)) / (1-insurance.unEmployedPercent/100)
-            }
-            return incomeBeforeTax + totalReduction + maximumInsuranceBaseOnMinimumWage * ((insurance.socialPercent + insurance.healthPercent)/100) + insurance.region.maximumUnemployedInsurance
+                return (incomeBeforeTax + totalReduction) / (1-((socialInsurancePercent + healthInsurancePercent + unEmployedInsurancePercent)/100))
+            }else{
+                if(netSalary < maxGrossBaseOnMaximumUnEmployed){
+                    return (incomeBeforeTax + totalReduction + maximumInsuranceBaseOnMinimumWage * ((socialInsurancePercent + healthInsurancePercent)/100)) / (1-unEmployedInsurancePercent/100)
+                }
+            }            
+            return incomeBeforeTax + totalReduction + maximumInsuranceBaseOnMinimumWage * ((socialInsurancePercent + healthInsurancePercent)/100) + insurance.region.maximumUnemployedInsurance
         }
-        return netSalary / (1-((insurance.socialPercent + insurance.healthPercent + insurance.unEmployedPercent)/100))
+        return netSalary / (1-((socialInsurancePercent + healthInsurancePercent + unEmployedInsurancePercent)/100))
     }else{
+        const other = +insurance.other
+        let socialInsurance = other * socialInsurancePercent / 100
+        let healthInsurance = other * healthInsurancePercent / 100
+        let unEmployedInsurance = other * unEmployedInsurancePercent / 100
         if(convertIncome > 0){
-            const maximumInsuranceBaseOnMinimumWage = insurance.minimumWage * defaultInsurance.factor
-            const incomeBeforeTax = Math.ceil(calculateIncomeBeforeTax(convertIncome))
-            if(valueToCalculateInsurance < maximumInsuranceBaseOnMinimumWage){
-                return (incomeBeforeTax + totalReduction) +  (valueToCalculateInsurance * ((insurance.socialPercent + insurance.healthPercent + insurance.unEmployedPercent)/100))
+            if(other > maximumInsuranceBaseOnMinimumWage){
+                socialInsurance =  maximumInsuranceBaseOnMinimumWage * socialInsurancePercent / 100
+                healthInsurance =  maximumInsuranceBaseOnMinimumWage * healthInsurancePercent / 100
+                if(other > maxGrossBaseOnMaximumUnEmployed){
+                    unEmployedInsurance = insurance.region.maximumUnemployedInsurance
+                    return incomeBeforeTax + totalReduction + socialInsurance + healthInsurance + unEmployedInsurance
+                }
             }
-            const maxGrossBaseOnMaximumUnEmployed = insurance.region.maximumUnemployedInsurance * 100
-            if(valueToCalculateInsurance < maxGrossBaseOnMaximumUnEmployed){
-                return (incomeBeforeTax + totalReduction + maximumInsuranceBaseOnMinimumWage * ((insurance.socialPercent + insurance.healthPercent)/100)) / (1-insurance.unEmployedPercent/100)
+            return incomeBeforeTax + totalReduction + socialInsurance + healthInsurance + unEmployedInsurance
+        }else{
+            if(other > maximumInsuranceBaseOnMinimumWage){
+                socialInsurance =  maximumInsuranceBaseOnMinimumWage * socialInsurancePercent / 100
+                healthInsurance =  maximumInsuranceBaseOnMinimumWage * healthInsurancePercent / 100
+                if(other > maxGrossBaseOnMaximumUnEmployed){
+                    unEmployedInsurance = insurance.region.maximumUnemployedInsurance
+                    return netSalary + socialInsurance + healthInsurance + unEmployedInsurance
+                }                
             }
-            return incomeBeforeTax + totalReduction + maximumInsuranceBaseOnMinimumWage * ((insurance.socialPercent + insurance.healthPercent)/100) + insurance.region.maximumUnemployedInsurance
+            return netSalary + socialInsurance + healthInsurance + unEmployedInsurance
         }
-        return netSalary + (valueToCalculateInsurance * ((insurance.socialPercent + insurance.healthPercent + insurance.unEmployedPercent)/100))
     }
 }
